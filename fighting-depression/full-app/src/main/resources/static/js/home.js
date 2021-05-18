cea66389602fb8d82a03007514fcf181da08799a
Avinash Mishra <avinash.mishra@nagarro.com>
2021-05-19 02:49:54 +0530

    scroll on nav

--- fighting-depression/full-app/src/main/resources/static/js/home.js
+++ fighting-depression/full-app/src/main/resources/static/js/home.js
@@ -19,6 +19,7 @@ var apiUrl="";
                $("#viewDayPageId").html("");
                $("#dashboardPageId").html("");
                $("#homePageDiv").show();
+               $("#homePageNav").show();
 
              },
             error: function (jqXHR) {
@@ -49,6 +50,7 @@ var apiUrl="";
                $("#viewDayPageId").html("");
                $("#dashboardPageId").html("");
                 $("#homePageDiv").hide();
+                $("#homePageNav").hide();
 
              },
             error: function (jqXHR) {
@@ -77,6 +79,7 @@ var apiUrl="";
           $("#viewDayPageId").html(jqXHR);
           $("#dashboardPageId").html("");
            $("#homePageDiv").hide();
+           $("#homePageNav").hide();
 
         },
        error: function (jqXHR) {
@@ -104,6 +107,7 @@ var apiUrl="";
           $("#viewDayPageId").html("");
            $("#dashboardPageId").html(jqXHR);
             $("#homePageDiv").hide();
+            $("#homePageNav").hide();
         },
        error: function (jqXHR) {
         alert(jqXHR.responseJSON.message);
@@ -231,4 +235,33 @@ if($("#authTokenId").val()!=undefined){
    window.sessionStorage.setItem("authTokenId", authToken);
 
    }
+
+
+     $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+
+      // Make sure this.hash has a value before overriding default behavior
+     if (this.hash !== "") {
+
+       // Prevent default anchor click behavior
+       event.preventDefault();
+
+       // Store hash
+       var hash = this.hash;
+
+       // Using jQuery's animate() method to add smooth page scroll
+       // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+       $('html, body').animate({
+         scrollTop: $(hash).offset().top
+       }, 900, function(){
+
+         // Add hash (#) to URL when done scrolling (default click behavior)
+         window.location.hash = hash;
+         });
+       } // End if
+     });
+
+
 });
+
+
+
